906037c10b5c8e4b55b7a5105007d84892c3f278
yigith <yigith1@gmail.com>
2023-01-11 17:14:27 +0300

    Add project files.

---
+++ PhotoBlog/Areas/Admin/Models/PostViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PhotoBlog.Areas.Admin.Models
+{
+    public class PostViewModel
+    {
+        public int Id { get; set; }
+
+        [MaxLength(140)]
+        public string Title { get; set; } = null!;
+
+        [MaxLength(400)]
+        public string? Description { get; set; }
+
+        public IFormFile Photo { get; set; } = null!;
+    }
+}
